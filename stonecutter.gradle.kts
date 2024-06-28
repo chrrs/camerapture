767f4cc49433861db75b44dad3a84cfc8df68c54
Chris <git@chrr.me>
2024-06-28 16:55:34 +0200

    chore: add an option to only build a small set of versions

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -4,12 +4,19 @@ plugins {
 
 stonecutter active "1.20.4" /* [SC] DO NOT EDIT */
 
+// Read the versions from CHISELED_VERSIONS, and only build / publish those versions.
+val chiseledVersions = providers.environmentVariable("CHISELED_VERSIONS")
+    .orNull?.split(",")
+val chiseledProjects = stonecutter.versions.filter { chiseledVersions == null || it.version in chiseledVersions }
+
 stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) {
+    versions.set(chiseledProjects)
     group = "project"
     ofTask("build")
 }
 
 stonecutter registerChiseled tasks.register("chiseledPublish", stonecutter.chiseled) {
+    versions.set(chiseledProjects)
     group = "project"
     ofTask("publishMods")
 }
