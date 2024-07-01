4062f6e2426e7d69566af6f1396b85da2010b7e8
Chris <git@chrr.me>
2024-07-01 17:56:45 +0200

    ci: manually trigger publishing of versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.4.1"
+    id("dev.kikugie.stonecutter") version "0.4.2"
 }
 
 extensions.configure<StonecutterSettings> {
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -5,9 +5,11 @@ plugins {
 stonecutter active "1.21" /* [SC] DO NOT EDIT */
 
 // Read the versions from CHISELED_VERSIONS, and only build / publish those versions.
+// If it's blank, we build / publish all available versions.
 val chiseledVersions = providers.environmentVariable("CHISELED_VERSIONS")
-    .orNull?.split(",")
-val chiseledProjects = stonecutter.versions.filter { chiseledVersions == null || it.version in chiseledVersions }
+    .orNull?.ifBlank { null }?.split(",")
+val chiseledProjects = stonecutter.versions
+    .filter { chiseledVersions?.contains(it.version) ?: true }
 
 stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) {
     versions.set(chiseledProjects)
