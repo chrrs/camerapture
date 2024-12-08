0718b1b204b8678caf2222680418f0a885f98207
chrrs <git@chrr.me>
2024-12-08 10:40:45 +0100

    chore: add a publishing workflow to Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import me.modmuss50.mpp.ReleaseType
 import net.fabricmc.loom.api.LoomGradleExtensionAPI
 import net.fabricmc.loom.task.RemapJarTask
+import org.gradle.internal.extensions.stdlib.capitalized
 
 plugins {
     id("architectury-plugin") version "3.4-SNAPSHOT"
     id("dev.architectury.loom") version "1.7-SNAPSHOT" apply false
     id("com.gradleup.shadow") version "9.0.0-beta2" apply false
+    id("me.modmuss50.mod-publish-plugin") version "0.8.1"
 }
 
 fun Project.hasProp(namespace: String, key: String) = hasProperty("$namespace.$key")
@@ -18,14 +21,19 @@ group = prop("mod", "group")
 version = "${prop("mod", "version")}+mc${versions.last()}"
 
 tasks {
-    // Create a new task `collect` that copies all jars to a common output directory.
-    val collect by registering(Copy::class) {
-        val tasks = subprojects.filter { it.path != ":common" }.map { it.tasks.named("remapJar") }
+    // Create a new task `buildAll` that builds and copies all jars to a common output directory.
+    val buildAll by registering(Copy::class) {
+        group = "build"
 
+        val tasks = subprojects.filter { it.path != ":common" }.map { it.tasks.named("remapJar") }
         dependsOn(tasks)
+
         from(tasks)
         into(layout.buildDirectory.dir("libs"))
     }
+
+    // We call buildAll to make sure everything is built before publishing.
+    publishMods.get().dependsOn(buildAll)
 }
 
 subprojects {
@@ -81,3 +89,72 @@ subprojects {
     }
 }
 
+publishMods {
+    changelog.set(providers.environmentVariable("CHANGELOG"))
+    type.set(if (prop("mod", "version").contains("beta")) ReleaseType.BETA else ReleaseType.STABLE)
+
+    /// Generate some common options between Modrinth and CurseForge publishing.
+    fun platformOptions(platform: String) = publishOptions {
+        val project = project(":$platform")
+        val name = if (platform == "neoforge") "NeoForge" else platform.capitalized()
+
+        displayName.set("${prop("mod", "version")} - $name ${versions.last()}")
+        version.set(project.version.toString())
+        modLoaders.addAll(project.prop("platform", "loaders").split(","))
+        file.set(project.tasks.getByName<RemapJarTask>("remapJar").archiveFile)
+    }.get()
+
+    modrinth("modrinthFabric") {
+        projectId.set(prop("modrinth", "id"))
+        accessToken.set(providers.environmentVariable("MODRINTH_TOKEN"))
+
+        from(platformOptions("fabric"))
+        minecraftVersions.addAll(versions)
+        requires("fabric-api")
+        optional("cloth-config")
+    }
+
+    modrinth("modrinthNeoForge") {
+        projectId.set(prop("modrinth", "id"))
+        accessToken.set(providers.environmentVariable("MODRINTH_TOKEN"))
+
+        from(platformOptions("neoforge"))
+        minecraftVersions.addAll(versions)
+        optional("cloth-config")
+    }
+
+    curseforge("curseforgeFabric") {
+        projectId.set(prop("curseforge", "id"))
+        accessToken.set(providers.environmentVariable("CURSEFORGE_TOKEN"))
+
+        from(platformOptions("fabric"))
+        minecraftVersions.addAll(versions)
+        requires("fabric-api")
+        optional("cloth-config")
+    }
+
+    curseforge("curseforgeNeoForge") {
+        projectId.set(prop("curseforge", "id"))
+        accessToken.set(providers.environmentVariable("CURSEFORGE_TOKEN"))
+
+        from(platformOptions("neoforge"))
+        minecraftVersions.addAll(versions)
+        optional("cloth-config")
+    }
+
+    github {
+        repository.set(prop("github", "repository"))
+        accessToken = providers.environmentVariable("GITHUB_TOKEN")
+
+        tagName.set("v${prop("mod", "version")}")
+        displayName.set(tagName)
+        commitish.set(tagName)
+
+        allowEmptyFiles.set(true)
+        additionalFiles.from(subprojects.filter { it.path != ":common" }
+            .map { it.tasks.getByName<RemapJarTask>("remapJar") }
+            .map { it.archiveFile })
+    }
+
+    dryRun.set(true)
+}
