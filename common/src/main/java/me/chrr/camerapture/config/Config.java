6fa3b0e4bf666143a29fc0535f64c1284177c6a1
chrrs <git@chrr.me>
2025-02-22 15:07:52 +0100

    refactor: let GSON handle config field capitalization

--- common/src/main/java/me/chrr/camerapture/config/Config.java
+++ common/src/main/java/me/chrr/camerapture/config/Config.java
@@ -1,54 +1,35 @@
 package me.chrr.camerapture.config;
 
-import com.google.gson.annotations.SerializedName;
 
 public class Config {
+    public static Config DEFAULT = new Config();
+
     public Client client = new Client();
     public Server server = new Server();
 
     public static class Client {
-        public static int VERSION = 2;
+        public int version = 2;
 
-        @SerializedName("version")
-        public int version = VERSION;
-
-        @SerializedName("cache_pictures")
         public boolean cachePictures = false;
-
-        @SerializedName("save_screenshot")
         public boolean saveScreenshot = false;
 
         public void upgrade() {
-            this.version = VERSION;
+            this.version = DEFAULT.client.version;
         }
     }
 
     public static class Server {
-        public static int VERSION = 4;
+        public int version = 4;
 
-        @SerializedName("version")
-        public int version = VERSION;
-
-        @SerializedName("max_image_bytes")
         public int maxImageBytes = 500_000;
-
-        @SerializedName("max_image_resolution")
         public int maxImageResolution = 1920;
-
-        @SerializedName("ms_per_picture")
         public int msPerPicture = 20;
-
-        @SerializedName("can_rotate_pictures")
         public boolean canRotatePictures = true;
-
-        @SerializedName("check_frame_position")
         public boolean checkFramePosition = false;
-
-        @SerializedName("allow_uploading")
         public boolean allowUploading = true;
 
         public void upgrade() {
-            this.version = VERSION;
+            this.version = DEFAULT.server.version;
         }
     }
 }
