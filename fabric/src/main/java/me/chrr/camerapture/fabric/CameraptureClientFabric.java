42136ff789265d28abfe4ac33342307cc3a86578
chrrs <git@chrr.me>
2025-05-01 13:46:07 +0200

    fix: only process a single image per tick on the client-side.
    Fixes #54

--- fabric/src/main/java/me/chrr/camerapture/fabric/CameraptureClientFabric.java
+++ fabric/src/main/java/me/chrr/camerapture/fabric/CameraptureClientFabric.java
@@ -11,6 +11,7 @@ import me.chrr.camerapture.render.PictureFrameEntityRenderer;
 import me.chrr.camerapture.render.PictureItemRenderer;
 import me.chrr.camerapture.render.ShouldRenderPicture;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.event.client.player.ClientPreAttackCallback;
@@ -75,5 +76,9 @@ public class CameraptureClientFabric implements ClientModInitializer {
             ClientPictureStore.getInstance().clear();
             CameraptureClient.syncedConfig = SyncedConfig.fromServerConfig(Camerapture.CONFIG_MANAGER.getConfig().server);
         });
+
+        // Process any received pictures once per tick.
+        ClientTickEvents.START_CLIENT_TICK.register((world) ->
+                ClientPictureStore.getInstance().processQueue());
     }
 }
