ea29fbb316034d48e01104781811189218261eae
chrrs <git@chrr.me>
2025-06-17 19:08:44 +0200

    feat: update to 1.21.6

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -21,9 +21,6 @@ val common: Configuration by configurations.creating {
 
 repositories {
     maven("https://maven.neoforged.net/releases/")
-
-    // FIXME: remove this when 1.21.6 gets merged.
-    maven("https://prmaven.neoforged.net/NeoForge/pr2297")
 }
 
 dependencies {
